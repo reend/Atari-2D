2e955d4d1ec942a7e06238c8f52dc8f0b8906c80
K.P <prilipko.k@yoju.casino>
2023-03-11 19:28:15 +0100

    refactoring

--- main.asm
+++ main.asm
@@ -1,18 +1,25 @@
 	org $2000
 
-charset = $3c00 ; Character Set
-screen = $4000  ; Screen buffer
+map     = $3000 ; Map
+charset = $4000 ; Character Set
+pmg     = $5000 ; Player Missile Data
+canvas  = $6000 ; Screen buffer
 
 	setup_screen()
 	setup_colors()
-	load_gfx()
-	display_map()
+	mva #>charset CHBAS
+	clear_pmg()
+	load_pmg()
+	setup_pmg()
+	copy_map_to_canvas()
 
 	jmp *
 
 	icl 'hardware.asm'
 	icl 'dlist.asm'
 	icl 'gfx.asm'
+	icl 'pmgdata.asm'
+	icl 'map.asm'
 
 * --------------------------------------- *
 * Proc: setup_colors                      *
@@ -24,71 +31,140 @@ lt_gray = $0a
 green = $c2
 brown = $22
 black = $00
+peach = $2c
+blue = $80
 
+	; Character Set Colors
 	mva #med_gray COLOR0 ; %01
 	mva #lt_gray COLOR1  ; %10
 	mva #green COLOR2	 ; %11
 	mva #brown COLOR3    ; %11 (inverse)
 	mva #black COLOR4    ; %00
+
+	; Player-Missile Colors
+	mva #brown PCOLR0
+	mva #peach PCOLR1
+	mva #blue PCOLR2
+	mva #black PCOLR3
+
 	rts
 	.endp
 
 * --------------------------------------- *
-* Proc: load_gfx                          *
-* Loads graphics into character set       *
+* Proc: clear_pmg                         *
+* Clears memory for Player-Missile Gfx    *
 * --------------------------------------- *
-.proc load_gfx
-	mva #>charset CHBAS
+.proc clear_pmg
+pmg_p0 = pmg + $200
+pmg_p1 = pmg + $280
+pmg_p2 = pmg + $300
+pmg_p3 = pmg + $380
+
+	ldx #$80
+	lda #0
+loop
+	dex
+	sta pmg_p0,x
+	sta pmg_p1,x
+	sta pmg_p2,x
+	sta pmg_p3,x
+	bne loop
+	rts
+	.endp
+
+* --------------------------------------- *
+* Proc: load_pmg                          *
+* Load PMG Graphics                       *
+* --------------------------------------- *
+.proc load_pmg
+pmg_p0 = pmg + $200
+pmg_p1 = pmg + $280
+pmg_p2 = pmg + $300
+pmg_p3 = pmg + $380
 
 	ldx #0
 loop
-	mva gfx,x charset,x
-	mva gfx+8,x charset+8,x
-	mva gfx+16,x charset+16,x
-	mva gfx+32,x charset+32,x
-	
+	mva pmgdata,x pmg_p0+64,x
+	mva pmgdata+8,x pmg_p1+64,x
+	mva pmgdata+16,x pmg_p2+64,x
+	mva pmgdata+24,x pmg_p3+64,x
 	inx
-	cpx #128
+	cpx #8
 	bne loop
 	rts
 	.endp
 
 * --------------------------------------- *
-* Proc: display_map                       *
-* Displays the current map                *
+* Proc: setup_pmg                         *
+* Sets up Player-Missile Graphics System  *
 * --------------------------------------- *
-.proc display_map
+.proc setup_pmg
+	mva #>pmg PMBASE
+	mva #46 SDMCTL ; Single Line resolution
+	mva #3 GRACTL  ; Enable PMG
+	mva #1 GRPRIOR ; Give players priority
+	lda #120
+	sta HPOSP0
+	sta HPOSP1
+	sta HPOSP2
+	sta HPOSP3
+	rts
+	.endp
+
+* --------------------------------------- *
+* Macro: blit_row                         *
+* Copies one line of map to screen        *
+* --------------------------------------- *
+.macro blit_row map, screen
+	ldx #0
 	ldy #0
 loop
-	mva map,y screen,y
-	mva map+40,y screen+40,y
-	mva map+80,y screen+80,y
-	mva map+120,y screen+120,y
-	mva map+160,y screen+160,y
-	mva map+200,y screen+200,y
-	mva map+240,y screen+240,y
-	mva map+280,y screen+280,y
-	mva map+320,y screen+320,y
-	mva map+360,y screen+360,y
-	mva map+400,y screen+400,y
-	mva map+440,y screen+440,y
-
+	lda :map,x
+	asl
+	sta :screen,y
 	iny
-	cpy #40
+	clc
+	adc #1
+	sta :screen,y
+	iny
+	inx
+	cpx #20
 	bne loop
+	.endm
+
+* --------------------------------------- *
+* Proc: copy_map_to_canvas                *
+* Copies map to canvas with interpolation *
+* --------------------------------------- *
+.proc copy_map_to_canvas
+map_ptr = $92
+canvas_ptr = $94
+
+	mwa #map map_ptr
+	mwa #canvas canvas_ptr
+
+	ldy #0
+loop
+	lda (map_ptr),y
+	asl
+	sta (canvas_ptr),y
+
+	inc canvas_ptr
+	bne next
+	inc canvas_ptr+1
+
+next
+	add #1
+	sta (canvas_ptr),y
+	iny
+	bne loop
+
+	inc map_ptr+1
+	inc canvas_ptr+1
+
+	lda map_ptr+1
+	cmp #>(map + $1000)
+	bne loop
+
 	rts
-	
-map
-	.byte 2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3
-	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
-	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
-	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
-	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
-	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
-	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
-	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3,2,3,6,7,2,3,2,3
-	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3,4,5,4,5,4,5,2,3
-	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3,4,5,10,11,4,5,2,3
-	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3,4,5,4,5,4,5,2,3
-	.byte 2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3
 	.endp
